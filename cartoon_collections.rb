773601edada7166bdfe6117d369af84badc39036
Janice Chan <janice.chan1288@gmail.com>
2022-06-15 11:54:04 -0400

    commit tree c2e309c53b5ae5d96b16fd2810c9485e93d301f3

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name_array)
+  name_array.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
+end
+#roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+
+
+def summon_captain_planet(planeteers)
+  planeteers.map do |planeteer|
+    planeteer.capitalize + "!"
+    end
+end
+# planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+# summon_captain_planet(planeteer_calls)
+
+
+def long_planeteer_calls(call_array)
+  call_array.any? do |call|
+    call.length > 4
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  snacks.find do |snack|
+  cheese_types.include?(snack)
+  end
 end
+
+# snacks = ["crackers", "gouda", "thyme"]
+# find_the_cheese(snacks)
